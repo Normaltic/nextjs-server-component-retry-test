164c1be28edab2f04e30979a82523b9fded523a9
Yunji Kim <normaltic@gmail.com>
2025-04-18 21:05:59 +0900

    Add additional RandomNumber

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,15 @@ import RandomNumber from "@/components/RandomNumber";
 export default async function Home() {
   return (
     <div>
-      <main className="grid grid-cols-2 gap-4 h-80 p-4">
+      <main className="grid grid-cols-2 gap-4 h-110 p-4">
         <div className="col-span-2 bg-green-400"></div>
         <div className="col-span-1 bg-green-400"></div>
         <div className="col-span-1 border-1">
+          <ErrorBoundary Fallback={Fallback}>
+            <RandomNumber className="w-full h-full flex justify-center items-center" />
+          </ErrorBoundary>
+        </div>
+        <div className="col-span-2 border-1">
           <ErrorBoundary Fallback={Fallback}>
             <RandomNumber className="w-full h-full flex justify-center items-center" />
           </ErrorBoundary>

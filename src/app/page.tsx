139e8d2377c9b93269719c01afe2db7368232d4a
Yunji Kim <normaltic@gmail.com>
2025-04-18 14:29:23 +0900

    Apply Error page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
-export default function Home() {
+import RandomNumber from "@/components/RandomNumber";
+
+export default async function Home() {
   return (
     <div>
-      <main></main>
+      <main className="grid grid-cols-2 gap-4 h-80 p-4">
+        <div className="col-span-2 bg-green-400"></div>
+        <div className="col-span-1 bg-green-400"></div>
+        <div className="col-span-1 border-1">
+          <RandomNumber className="w-full h-full flex justify-center items-center border-1" />
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/components/RandomNumber.tsx
@@ -0,0 +1,19 @@
+export interface RandomNumberProps {
+  className?: string;
+}
+
+async function fetching() {
+  await new Promise<void>((res) => {
+    setTimeout(() => res(), 1500);
+  });
+
+  const rand = Math.floor(Math.random() * 10);
+  if (rand > 4) return rand;
+  else throw new Error("error");
+}
+
+export default async function RandomNumber({ className }: RandomNumberProps) {
+  const number = await fetching();
+
+  return <div className={className}>{number}</div>;
+}
